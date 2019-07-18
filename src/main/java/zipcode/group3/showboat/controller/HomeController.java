12994f06ae5b4a7629451343815e71ba2a9399bc
Ryan <grossry@oregonstate.edu>
2019-07-17 20:25:58 -0400

    Refactoring ping endpoint

--- src/main/java/zipcode/group3/showboat/controller/HomeController.java
+++ src/main/java/zipcode/group3/showboat/controller/HomeController.java
@@ -1,15 +1,26 @@
 package zipcode.group3.showboat.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    public String get() {
-        return "This is the home page";
+    public ResponseEntity ping() throws URISyntaxException
+    {
+        URI pingUri = new URI("https://http.cat/200");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(pingUri);
+        return new ResponseEntity<>(httpHeaders, HttpStatus.OK);
     }
 }
